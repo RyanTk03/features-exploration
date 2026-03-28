5a996e867ed4e3e43598a8d8b7228ae244755fa6
ryntk <rayanetoko@proton.me>
2026-03-28 19:19:28 +0100

    initialization

---
+++ README.md
@@ -0,0 +1,1 @@
+# Identification and evaluation of most relevant features for the detection of malicious Android applications using machine learning models
---
+++ main.py
@@ -0,0 +1,6 @@
+def main():
+    print("Hello from feature-reduction-in-malware-detection-with-machine-learning!")
+
+
+if __name__ == "__main__":
+    main()
